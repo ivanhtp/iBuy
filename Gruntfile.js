f805203fe29c36380b73909abcee73af3c302089
Ivan Pauletti <ivanpauletti@gmail.com>
2015-10-20 15:29:49 -0200

    Committing build folder for Github pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -119,7 +119,7 @@ module.exports = function (grunt) {
                     {
                         expand: true,
                         cwd: 'src/',
-                        src: '**/*.html',
+                        src: ['views/**/*.html', '*.html'],
                         dest: 'build'
                     }
                 ]
